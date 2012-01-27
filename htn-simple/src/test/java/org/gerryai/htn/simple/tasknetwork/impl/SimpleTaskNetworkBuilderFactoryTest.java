cc8e2630a8e316969b181d7207d8a91774ae3acb
David Edwards <david@more.fool.me.uk>
2012-01-27 08:00:35 +0000

    Simple unit test for task network builder factory

---
+++ htn-simple/src/test/java/org/gerryai/htn/simple/tasknetwork/impl/SimpleTaskNetworkBuilderFactoryTest.java
@@ -0,0 +1,39 @@
+/**
+ *  Gerry AI - Open framework for automated planning algorithms
+ *  Copyright (C) 2012  David Edwards
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.gerryai.htn.simple.tasknetwork.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author David Edwards <david@more.fool.me.uk>
+ *
+ */
+public class SimpleTaskNetworkBuilderFactoryTest {
+
+	/**
+	 * Test builder creation.
+	 */
+	@Test
+	public void testCreate() {
+		SimpleTaskNetworkBuilderFactory factory = new SimpleTaskNetworkBuilderFactory();
+		assertTrue(factory.create() instanceof SimpleTaskNetworkBuilder);
+	}
+
+}
